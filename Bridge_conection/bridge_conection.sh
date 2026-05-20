e47d4a9ec1cf83855d69fb25365236642cd806ad
user-root <usersolo1@protonmail.com>
2026-05-20 02:02:59 +0000

    Update get the ip's and check the bridge if already exist

--- Bridge_conection/bridge_conection.sh
+++ Bridge_conection/bridge_conection.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/zsh
 
-
-#for iface in /sys/class/net/*; do
-#    if [ -d "$iface/device" ]; then
-#			interface= basename "$iface"
-#			type=$(cat $iface/$interface/type)
-#			nics+=($interface)
-#    fi
-#done
-
 echo -e "\n[..] Creando el bridge 'br0' con la interfaz ethernet"
 
-if [ ! -d "/sys/class/net/br0" ]; then
+ipaddrcird=$(ip addr show enp58s0 | grep -E "inet " | awk '{print($2)}')
+iproute=$(ip route | grep "via" | awk '{print($3)}')
 
-	ipaddrcird=$(ip addr show enp58s0 | grep -E "inet " | awk '{print($2)}')
-	ipaddr=$(ip addr show enp58s0 | grep -E "inet " | awk '{print($2)}' | sed 's/\/24/ /')
-	iproute=$(ip route | grep "via" | awk '{print($3)}')
+if [ ! -d "/sys/class/net/br0" ]; then
 
 	ip link add name br0 type bridge
 	ip link set dev br0 up
